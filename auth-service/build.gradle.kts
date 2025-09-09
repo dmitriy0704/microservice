4d3050b033fd71c0fb107cfaa3d10c09b213b91c
Dmitriy Folomkin <dmitriy.folomkin@yandex.ru>
2025-09-09 21:14:06 +0300

    UPDATE: в сервисе auth-service удалена базовая авторизация для подключения авторизации с jwt токеном

--- auth-service/build.gradle.kts
+++ auth-service/build.gradle.kts
@@ -7,7 +7,7 @@ plugins {
 
 dependencies {
     implementation("org.springframework.boot:spring-boot-starter-security")
-    implementation("io.jsonwebtoken:jjwt-api:0.12.5") // Or a newer version
-    runtimeOnly("io.jsonwebtoken:jjwt-impl:0.12.5")
-    runtimeOnly("io.jsonwebtoken:jjwt-jackson:0.12.5")
+    implementation("io.jsonwebtoken:jjwt-api:0.13") // Or a newer version
+    runtimeOnly("io.jsonwebtoken:jjwt-impl:0.13")
+    runtimeOnly("io.jsonwebtoken:jjwt-jackson:0.13")
 }
--- auth-service/src/main/kotlin/dev/folomkin/authservice/model/Login.kt
+++
@@ -1,4 +0,0 @@
-package dev.folomkin.authservice.model
-
-data class LoginRequest(val username: String, val password: String)
-data class LoginResponse(val token: String)
